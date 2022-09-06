13d94bc4d0899b059dd7e5378922a8456608a9d9
ytugy <65300639+doodooboi@users.noreply.github.com>
2022-09-06 09:47:45 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -2,10 +2,21 @@ local function MakeGrid(Width, Height)
   local TestGrid = {}
   
   for Y = 1, Height do
-    TestGrid[y] = {}
+    TestGrid[Y] = {}
     for X = 1, Width do
-        TestGrid[y][x] = {255,255,255}
+        TestGrid[Y][X] = {255,255,255}
     end 
   end
+  
   return TestGrid
 end
+
+local OriginalGrid = MakeGrid(5,5)
+
+for Index, Value in ipairs(OriginalGrid) do
+  local blah = {}
+  for Value2, _ in ipairs(Value) do
+      table.insert(blah, Value2)
+  end 
+  print(Index, table.concat(blah, " "))
+end 
