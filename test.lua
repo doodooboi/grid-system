284193c634ab2f809b1b8dbb52e45d5272c13c76
ytugy <65300639+doodooboi@users.noreply.github.com>
2022-09-08 11:11:50 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,3 +1,6 @@
+local GridX = 5
+local GridY = 5
+
 local function MakeGrid(Width, Height)
   local TestGrid = {}
   
@@ -11,12 +14,50 @@ local function MakeGrid(Width, Height)
   return TestGrid
 end
 
-local OriginalGrid = MakeGrid(5,5)
-
-for Index, Value in ipairs(OriginalGrid) do
+local function PrintGrid(grid)
+  for Index, Value in ipairs(grid) do
   local blah = {}
   for Value2, _ in ipairs(Value) do
       table.insert(blah, Value2)
   end 
   print(Index, table.concat(blah, " "))
 end 
+end
+
+local OriginalGrid = MakeGrid(5,5)
+local Visited = {}
+
+local function GreedyMesh(StartY, StartX, RecursiveData)
+  RecursiveData = RecursiveData or {}
+  
+  local SizeX = RecursiveData.SizeX or 0
+  local SizeY = RecursiveData.SizeY or 0
+
+  local Direction = RecursiveData.Direction or "X"
+
+  local CurrentCell = OriginalGrid[StartY][StartX]
+  local NeighboringCell = nil
+
+  if StartX + 1 > GridX then
+    if OriginalGrid[StartY+1] then
+      StartY = StartY+1
+      print("Going down")
+    else
+      print("Could not move down because Y"..StartY+1 .." does not exist")
+      return
+    end
+  elseif StartY + 1 > GridY then
+    print("Reached end of grid")
+    return RecursiveData
+  else
+    print("Moved over x")
+  end
+
+  if Direction == "X" then
+    NeighboringCell = OriginalGrid[StartY][StartX+1]
+  else
+    NeighboringCell = OriginalGrid[StartY+1][StartX]
+  end
+end
+
+GreedyMesh(4,4)
