e06d6a6cc7d0cc5ec53e6831e6d69164627d4b87
ytugy <65300639+doodooboi@users.noreply.github.com>
2022-09-08 21:31:44 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,106 +1,176 @@
-local GridX = 5
-local GridY = 5
+local GridX = 9
+local GridY = 9
 
 local function MakeGrid(Width, Height)
-  local TestGrid = {}
-  
-  for Y = 1, Height do
-    TestGrid[Y] = {}
-    for X = 1, Width do
-        TestGrid[Y][X] = {255,255,255}
-    end 
-  end
-  
-  return TestGrid
+	local TestGrid = {}
+
+	for Y = 1, Height do
+		TestGrid[Y] = {}
+		for X = 1, Width do
+			TestGrid[Y][X] = {1}
+		end 
+	end
+
+	return TestGrid
 end
 
 local function PrintGrid(grid)
-  for Index, Value in ipairs(grid) do
-  local blah = {}
-  for Value2, _ in ipairs(Value) do
-      table.insert(blah, Value2)
-  end 
-  print(Index, table.concat(blah, " "))
-end 
+	for Index, Value in ipairs(grid) do
+		local blah = {}
+		for Index2, Value2 in ipairs(Value) do
+			table.insert(blah, Value2[1])
+		end 
+		print(Index..":", table.concat(blah, " "))
+	end 
 end
 
-local OriginalGrid = MakeGrid(5,5)
+local OriginalGrid = MakeGrid(GridX,GridY)
+local Limit = 100
 local Visited = {}
 
 local function CellsAreSame(cell1, cell2) 
-  if #cell1 ~= #cell2 then return false end
-  
-  for Index, _ in ipairs(cell1) do
-    if cell1[Index] ~= cell2[Index] then return false end
-  end 
+	if #cell1 ~= #cell2 then return false end
 
-  return true
+	for Index, _ in ipairs(cell1) do
+		if cell1[Index] ~= cell2[Index] then return false end
+	end 
+
+	return true
 end
+
+local function SetVisited(StartY,StartX,CurrentY,CurrentX)
+	if not Visited[StartY] then
+		Visited[StartY] = {}
+	elseif not Visited[CurrentY] then
+		Visited[CurrentY] = {}
+	end
+
+	Visited[CurrentY][CurrentX] = true
+	Visited[StartY][StartX] = true
+end
+
+local function LimitXY()
 
 local function GreedyMesh(StartY, StartX, RecursiveData)
-  
-  RecursiveData = RecursiveData or {}
-  
-  local SizeX = RecursiveData.SizeX or 1
-  local SizeY = RecursiveData.SizeY or 1
 
-  local CurrentX = StartX
-  local CurrentY = StartY
-  
-  local Direction = "X"
+	RecursiveData = RecursiveData or {}
+	
+	if RecursiveData.Recurse and RecursiveData.Recurse > Limit then
+		warn("Recursion Limit")
+		return
+	end
+	
+	local MaxX = RecursiveData.XLimit or GridX
+	local MaxY = RecursiveData.YLimit or GridY
+	
+	local SizeX = RecursiveData.SizeX or 1
+	local SizeY = RecursiveData.SizeY or 1
 
-  local CurrentCell = OriginalGrid[CurrentY][CurrentX]
-  local NeighboringCell = nil
+	local CurrentX = StartX
+	local CurrentY = StartY
 
-  if StartX + 1 > GridX then
-    if OriginalGrid[CurrentY+1] then
-      CurrentY = StartY+1
-      Direction = "Y"
-     -- print("Moved Y")
-    else
-      print("Y"..CurrentY+1 .." does not exist")
-      return RecursiveData
-    end
-  elseif CurrentY + 1 > GridY then
-    print("Reached end of grid")
-    return RecursiveData
-  end
+	local Direction = "X"
 
-  if Direction == "X" then
-    NeighboringCell = OriginalGrid[CurrentY][CurrentX+1]
-   -- print("Moved X")
-  else
-    NeighboringCell = OriginalGrid[StartY+1][StartX]
-    --print("Moved Y")
-  end
+	local CurrentCell = OriginalGrid[CurrentY][CurrentX]
+	local NeighboringCell = nil
+	
+	if CurrentX + 1 > MaxX then
+		if OriginalGrid[CurrentY+1] then
+			CurrentY = StartY+1
+			CurrentX = RecursiveData.OriginalX or CurrentX
+			
+			Direction = "Y"
+		else
+			return RecursiveData
+		end
+	elseif CurrentY + 1 > MaxY and (CurrentX + 1 > MaxX) then
+		return RecursiveData
+	end
 
-  print(CurrentY, CurrentX)
+	if Direction == "X" then
+		CurrentX += 1
+	end
+	
+	NeighboringCell = OriginalGrid[CurrentY][CurrentX]
+	
+	--warn(StartY,StartX)
+	--warn(CurrentY, CurrentX)
+	--warn(Direction)
+	--warn(CurrentCell, NeighboringCell)
+	--warn(RecursiveData)
+	--print()
+	
+	if not CurrentCell or not NeighboringCell then
+		return RecursiveData
+	end
 
-  if not CurrentCell or not NeighboringCell then
-    print("Not Exist")
-    return RecursiveData
-  end
-  
-  if CellsAreSame(CurrentCell, NeighboringCell) then
-    if Direction == "X" then
-      SizeX = SizeX + 1
-    else
-      SizeY = SizeY + 1
-    end
+	if CellsAreSame(CurrentCell, NeighboringCell) then
+		if Direction == "X" then
+			SizeX += 1
+		elseif Direction == "Y" then
+			SizeY += 1
+		end
+		
+		SetVisited(StartY, StartX, CurrentY, CurrentX)
+		
+		SizeX = math.min(SizeX, MaxX)
+		SizeY = math.min(SizeY, MaxY)
+		
+		local RecurseData = {
+			["SizeY"] = SizeY,
+			["SizeX"] = SizeX,
+			["Recurse"] = if RecursiveData.Recurse then RecursiveData.Recurse + 1 else 1,
+			["XLimit"] = RecursiveData.XLimit,
+			["YLimit"] = RecursiveData.YLimit,
+			
+			["OriginalX"] = RecursiveData.OriginalX,
+		}
+		
+		
+		if not RecursiveData.OriginalX then -- Our first recursion
+			RecurseData["OriginalX"] = StartX
+		end
+	
+		return GreedyMesh(CurrentY, CurrentX, RecurseData)
+	else
+		local RecurseData = {
+			["SizeY"] = SizeY,
+			["SizeX"] = SizeX,
+			["Recurse"] = if RecursiveData.Recurse then RecursiveData.Recurse + 1 else 1,
+			["XLimit"] = CurrentX - 1,
+			["YLimit"] = RecursiveData.YLimit,
 
-    local RecurseData = {
-     ["SizeY"] = SizeY,
-     ["SizeX"] = SizeX
-    }
-    
-    return GreedyMesh(CurrentY, CurrentX, RecurseData)
-  else
-    print("Not Same")
-    return RecursiveData
-  end
+			["OriginalX"] = RecursiveData.OriginalX,
+		}
+		
+		if not RecursiveData.OriginalX then -- Our first recursion
+			RecurseData["OriginalX"] = StartX
+		end
+		
+		return GreedyMesh(CurrentY, CurrentX-1, RecurseData)
+	end
 end
---OriginalGrid[4][5] = {255,254,255}
 
-local Data = GreedyMesh(1,5)
+local FinishedGreedyMeshing = false
+local XOffset = 1
+local YOffset = 1
 
-print(Data.SizeY, Data.SizeX)
+OriginalGrid[3][5] = {0}
+
+PrintGrid(OriginalGrid)
+
+for Y = 1, GridY do
+	for X = 1, GridX do
+		if Visited[Y] and Visited[Y][X] then continue end
+		
+		local Data = GreedyMesh(Y,X)
+		
+		print(Y,X, Data)
+	end
+end
+
+--print(OriginalGrid)
+
+--local Data = GreedyMesh(1,1)
+
+--print(Data.SizeY, Data.SizeX)
